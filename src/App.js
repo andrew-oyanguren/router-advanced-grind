1849ab8e0d26d1cdfe5e34be3f5c53468dedb920
Andrew Oyanguren <andrewoyanguren@Andrews-MacBook-Pro.local>
2021-07-21 13:43:04 -0700

    Added Lazy Loading for all of the apps pages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
+import React, { Suspense } from 'react';
+
 import { Switch, Route, Redirect} from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
 
-// === Import Pages:
-import AllQuotes from './pages/AllQuotes/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail/QuoteDetail';
-import NewQuote from './pages/NewQuote/NewQuote';
-import NotFound from './pages/NotFound/NotFound';
+// Lazy Page Loading:
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes/AllQuotes'));
+const NewQuote = React.lazy(() => import('./pages/NewQuote/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound/NotFound'));
+
+// Lazy Loading Fallback
+const susFallback = (
+  <div className='centered'>
+    <LoadingSpinner />
+  </div>
+);
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
+      <Suspense fallback={susFallback}>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
 
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
+          <Route path='/quotes' exact>
+            <AllQuotes />
+          </Route>
 
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
 
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
 
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
--- src/pages/QuoteDetail/QuoteDetail.js
+++ src/pages/QuoteDetail/QuoteDetail.js
@@ -9,19 +9,6 @@ import LoadingSpinner from '../../components/UI/LoadingSpinner';
 import useHttp from '../../hooks/use-http';
 import { getSingleQuote } from '../../lib/api';
 
-const DUMMY_QUOTES = [
-  {
-    id: 'q1',
-    author: 'Andrew',
-    text: 'Learning React is fun!'
-  },
-  {
-    id: 'q2',
-    author: 'Rianna',
-    text: 'Mastering React is better'
-  }
-];
-
 const QuoteDetail = () => {
 
   const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
